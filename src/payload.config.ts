c700eaff718919a39e5d7aafdeedec4e96e16497
seangjr <sean@relampagos.org>
2022-12-07 12:44:30 +0800

    Added rate limit to requests

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -2,16 +2,24 @@ import { buildConfig } from 'payload/config';
 import path from 'path';
 // import Examples from './collections/Examples';
 import Users from './collections/Users';
+import Works from './collections/Works';
+import Details from './collections/Details';
+import Media from './collections/Media';
 
 export default buildConfig({
   serverURL: process.env.PAYLOAD_PUBLIC_BASE_DNS,
+  rateLimit: {
+    trustProxy: true,
+  },
   admin: {
     user: Users.slug,
   },
   collections: [
     Users,
     // Add Collections here
-    // Examples,
+    Works,
+    Details,
+    Media 
   ],
   typescript: {
     outputFile: path.resolve(__dirname, 'payload-types.ts'),
